cf1bab00b5c0255c2b9a70cc8b782ab7621506a7
iansdu <32809772@qq.com>
2022-03-17 16:14:58 +0800

    0317.1

---
+++ test.js
@@ -0,0 +1,25 @@
+function foo() {
+    bar();
+}
+
+function bar(a) {
+    console.info()
+}
+
+function foo() {
+    bar("Hello World");
+}
+
+function bar() {
+    console.log('bar');
+}
+
+for (var i = 0; i < 5; i++) {
+console.log('i');
+console.sqrt('i');
+console.sqrt('j');
+bar
+console.sqrt('j');
+bar
+
+}
---
+++ test.py
@@ -0,0 +1,32 @@
+class gt:
+    
+    def __init__(self):
+        self.a = 21
+
+        # region b
+        self.b = 34
+        # end region
+class gt:
+    
+    def __init__(self):
+        self.a = 21
+
+        # region b
+        self.b = 34
+        # end region
+class gt:
+    
+    def __init__(self):
+        self.a = 21
+
+        # region b
+        self.b = 34
+        # end region
+class gt:
+    
+    def __init__(self):
+        self.a = 21
+
+        # region b
+        self.b = 34
+        # end region
