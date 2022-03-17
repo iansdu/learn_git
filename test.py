bc87e44e13b90f79546bbe8da00e53432a74d20e
iansdu <32809772@qq.com>
2022-03-17 16:52:01 +0800

    0317.2

--- test.py
+++ test.py
@@ -17,16 +17,12 @@ class gt:
 class gt:
     
     def __init__(self):
-        self.a = 21
+        self.a = 214
 
         # region b
-        self.b = 34
+        self.b = 3
         # end region
-class gt:
-    
-    def __init__(self):
-        self.a = 21
-
-        # region b
-        self.b = 34
-        # end region
+def foo(a, b):
+    a = 12
+    b = 23
+    return a + b
